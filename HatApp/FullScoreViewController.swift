848953e1f0f7209248b3127f7fc0a31b9a11e190
Denis Makovets <dmakovets@gmail.com>
2019-12-27 15:19:29 +0300

    hat app 62%

---
+++ HatApp/FullScoreViewController.swift
@@ -0,0 +1,22 @@
+//
+//  FullScoreViewController.swift
+//  HatApp
+//
+//  Created by Denis Makovets on 12/27/19.
+//  Copyright © 2019 Denis Makovets. All rights reserved.
+//
+
+import UIKit
+
+class FullScoreViewController: UIViewController {
+
+
+    @IBOutlet weak var firstTeam: UILabel!
+    @IBOutlet weak var secondTeam: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+   
+    }
+    
+}
